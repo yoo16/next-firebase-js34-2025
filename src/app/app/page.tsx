d4ac4e60daacd065beba87f425522a8b3f2b5b3f
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-09-17 12:27:13 +0900

    update

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -5,15 +5,18 @@ import TodoList from '@/components/TodoList';
 import Link from 'next/link';
 
 export default function AppPage() {
-    // const { user, loading } = useAuth();
+    // Firebase にログイン接続情報を取得
+    // loading は現在、Firebase で処理中かどうか？
+    const { user, loading } = useAuth();
 
-    // if (loading) return <p>Loading...</p>;
-    // if (!user) return (
-    //     <div className="space-y-3">
-    //         <p>このページはログインが必要です。</p>
-    //         <Link href="/" className="underline">トップへ戻る</Link>
-    //     </div>
-    // );
+    if (loading) return <p>Loading...</p>;
+    // ユーザがログインしていない場合は、ログインを促す
+    if (!user) return (
+        <div className="space-y-3">
+            <p>このページはログインが必要です。</p>
+            <Link href="/" className="underline">トップへ戻る</Link>
+        </div>
+    );
 
     return (
         <section className="space-y-6">

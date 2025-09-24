a8a48823287af52acba38ab5798022f72a354db7
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-09-24 11:33:47 +0900

    update

--- src/lib/saveUserProfile.ts
+++ src/lib/saveUserProfile.ts
@@ -9,7 +9,16 @@ export const saveUserToFirestore = async (
     uid: string,
     data: Omit<UserProfile, "uid" | "createdAt" | "updatedAt">
 ) => {
+    // users コレクションの uid ドキュメントを参照
     const ref = doc(db(), "users", uid);
+    // データをセット（存在しない場合は作成、存在する場合は更新）
+    // merge: true を指定すると、既存のフィールドを保持しつつ更新できる
+
+    // setDoc(データベースの参照, データ, オプション)
+
+    // SQLでいうところの
+    // INSERT INTO users (uid, displayName, email, photoURL, name, birthday, profile, createdAt, updatedAt)
+    // UPDATE users SET displayName = ..., email = ..., photoURL = ..., name = ..., birthday = ..., profile = ..., updatedAt = ... WHERE uid = ...
     await setDoc(
         ref,
         {
